defadba30c07a6b07ed0318fb72c239a0d862076
ourlord <loli.lawliet@gmail.com>
2014-08-27 16:06:57 -0400

    add some comments

--- PxPrinter.py
+++ PxPrinter.py
@@ -1,8 +1,12 @@
 import gdb
 
+# flag for using superb print
 superb = False
+# flag for using test print
+test = False
 
 def printInnerSuperb(v, c, level = 0):
+    # advanced version of print, will iterate into each member until we hit the ground
     indent = ' ' * level * 4
     # reach the end of iteration, just write that value and return
     if v.type.keys() == None:
@@ -30,6 +34,8 @@ def printInnerSuperb(v, c, level = 0):
             gdb.write('%s}\n' % (indent,))
 
 def printInner(v, c):
+    # this print just give the normal behavior of this script.
+    # won't iterate into each member of the passin object
     if c == None:
         for k in v.type.keys():
             try:
@@ -39,6 +45,10 @@ def printInner(v, c):
             gdb.write('%s = { %s }\n' % (k, v_))
     else:
         gdb.write('%s = { %s }\n' % (c, v[c]))
+
+def printInnerTest(v):
+    # test print method helps develop this script
+    gdb.write('%s\n' % (v.cast(v['px'].type.strip_typedefs()),))
 
 class PxPrinter(gdb.Command):
     """
@@ -64,9 +74,12 @@ class PxPrinter(gdb.Command):
             v = gdb.parse_and_eval(expr)
         except gdb.error, e:
             raise gdb.GdbError(e.message)
-        if superb:
-            printInnerSuperb(v, component)
+        if test:
+            printInnerTest(v)
         else:
-            printInner(v, component)
+            if superb:
+                printInnerSuperb(v, component)
+            else:
+                printInner(v, component)
 
 PxPrinter()
