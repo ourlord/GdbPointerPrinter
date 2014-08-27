587a7d71a0ffedadee4366c4601fde2551318adb
ourlord <loli.lawliet@gmail.com>
2014-08-27 13:57:53 -0400

    First version prototype

---
+++ PxPrinter.py
@@ -0,0 +1,30 @@
+import gdb
+
+def printHelper(v, c):
+    if c == None:
+        for k in v.type.keys():
+            try:
+                v_ = v[k]
+            except gdb.error:
+                continue
+            gdb.write('%s: %s \n' % (k, v_))
+    else:
+        gdb.write('%s: %s \n' % (c, v[c]))
+
+class PxPrinter(gdb.Command):
+    def __init__(self):
+        super(TestPrinter, self).__init__(
+                'ppx', gdb.COMMAND_DATA, gdb.COMPLETE_SYMBOL, False)
+    def invoke(self, arg, from_tty):
+        space_location = arg.find(' ')
+        if space_location == -1:
+            (expr, component) = (arg, None)
+        else:
+            (expr, component) = (arg[:space_location], arg[space_location+1:])
+        try:
+            v = gdb.parse_and_eval(expr)
+        except gdb.error, e:
+            raise gdb.GdbError(e.message)
+        printHelper(v, component)
+
+PxPrinter()
