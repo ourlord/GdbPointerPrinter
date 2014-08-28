52cc2a57198a65a6231bae8ed796c9ba176c7210
ourlord <loli.lawliet@gmail.com>
2014-08-28 13:50:53 -0400

    First release version of this script

--- PxPrinter.py
+++ PxPrinter.py
@@ -1,20 +1,38 @@
+#
+#   Author: Lincoln Xiong
+#   Github account: ourlord
+#   Contact: loli.lawliet@gmail.com
+#   Project link:https://github.com/ourlord/GdbPointerPrinter/
+#
+#   Usage: Help to print pointer from address in GDB, especially when dealing with a
+#   core-file(because you cannot run the process to do some dynamic stuffs).
+#   
+#   Example of using this command in GDB:
+#
+#   ppx <ptr_variable> [<member you concern about>]
+#
+
 import gdb
 
 def printInner(v, c):
-    # this print just give the normal behavior of this script.
-    # won't iterate into each member of the passin object
+    # if this pointer is a boost smart pointer, we only cares about the member px
     if 'px' in v.type.keys():
         v = v['px']
-        v = v.cast(v.type).dereference()
+    v = v.dereference()
     if c == None:
+        # iterate into the keys, print every member in the object
         for k in v.type.keys():
+            # some member actually don't hit anything, we just ignore them
             try:
                 v_ = v[k]
             except gdb.error:
                 continue
+            # use the encode in case there is any character issue
             gdb.write( ( '%s = { %s }\n' % (k, v_) ).encode('utf-8') )
     else:
+        # we only care about member c at this time
         try:
+            # use the encode in case there is any character issue
             gdb.write( ( '%s = { %s }\n' % (c, v[c]) ).encode('utf-8') )
         except gdb.error, e:
             gdb.GdbError(e.message)
@@ -24,7 +42,7 @@ class PxPrinter(gdb.Command):
     PxPrinter: a printer helps you to print out the content of a pointer invoke
     core-file especially. The command takes one optional argument, the component
     of that object you need to check.
-        ppx *<pointer_variable>.px [member class/paramter/method]
+        ppx <pointer_variable> [member class/paramter/method]
     """
     def __init__(self):
         super(PxPrinter, self).__init__(
